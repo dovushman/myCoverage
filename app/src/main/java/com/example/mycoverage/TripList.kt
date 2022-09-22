cd022687e17857acb35afcad6fa8fad7c324ed91
dovushman <52638904+dovushman@users.noreply.github.com>
2022-09-22 18:07:19 -0400

    remove autofilled trips

--- app/src/main/java/com/example/mycoverage/TripList.kt
+++ app/src/main/java/com/example/mycoverage/TripList.kt
@@ -4,24 +4,24 @@ package com.example.mycoverage
  * returns the initial set of Homework
  * useful for initialize the RecyclerView.Adapter
  */
-fun getTripList(): List<Trip> {
-
-    var steps: String
-    var location: String
-    var date: String
-    val trip = ArrayList<Trip>()
-
-    for (i in 1..20){
-
-        // this is one way to use getString outside any activity context
-        steps = BaseApplication.getAppContext().getString(R.string.steps, "12164")
-        location = BaseApplication.getAppContext().getString(R.string.location, "WPI")
-        date = BaseApplication.getAppContext().getString(R.string.date, "09/19/2022")
-
-        trip.add(
-            Trip(steps, location, date)
-        )
-    }
-
-    return trip
-}
+//fun getTripList(): List<Trip> {
+//
+//    var steps: String
+//    var location: String
+//    var date: String
+//    val trip = ArrayList<Trip>()
+//
+//    for (i in 1..20){
+//
+//        // this is one way to use getString outside any activity context
+//        steps = BaseApplication.getAppContext().getString(R.string.steps, "12164")
+//        location = BaseApplication.getAppContext().getString(R.string.location, "WPI")
+//        date = BaseApplication.getAppContext().getString(R.string.date, "09/19/2022")
+//
+//        trip.add(
+//            Trip(steps, location, date)
+//        )
+//    }
+//
+//    return trip
+//}
